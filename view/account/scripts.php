fe226d853a3a399a44e9f7cf0ed4253df009cba5
Tuấn Nguyễn <nguyenaituan95@gmail.com>
2019-12-19 14:55:46 +0700

    remove file not need

--- view/account/scripts.php
+++
@@ -1,7 +0,0 @@
-<script src="<?=base_url?>/js/modelo.js"></script>
-<?php if(count($view_data['errors']) > 0) { ?>
-    <script>
-        $('#modal-errors').modelo();
-        $("#openModal").click();
-    </script>
-<?php } ?>
