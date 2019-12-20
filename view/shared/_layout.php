1d0af9198a0ea641fc4f00776e74693920aadc7a
Tuấn Nguyễn <nguyenaituan95@gmail.com>
2019-12-20 22:35:20 +0700

    updated

--- view/shared/_layout.php
+++ view/shared/_layout.php
@@ -9,7 +9,7 @@
         <?php if(isset($view_data['section_meta'])) { 
             include 'view/' . $view_data['section_meta'];
         } ?>
-        <link href="<?= base_url ?>/css/style2.css?v=4" rel="stylesheet">
+        <link href="<?= base_url ?>/css/style2.css?v=5" rel="stylesheet">
         <link href="<?= base_url ?>/css/custom.css?v=2" rel="stylesheet">
         <link href="<?= base_url ?>/css/icon.css?v=1" rel="stylesheet">
         <link href="<?= base_url ?>/assets/fc340a4a/css/kv-widgets.min.css" rel="stylesheet">
